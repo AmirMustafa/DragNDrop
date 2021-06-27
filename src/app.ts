5900d012f9b8e38ee3700dada768fe12e2f58cde
Amir Mustafa <amir.mustafa@xebia.com>
2021-06-27 21:42:02 +0530

    fix: optimized people description with getter

--- src/app.ts
+++ src/app.ts
@@ -178,6 +178,15 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 // Making ProjectItem reusable
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
+
+  // getter is called without parantheses - check - i.e. this.person
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, true, project.id);
     this.project = project;
@@ -188,13 +197,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
   configure() {}
   renderContent() {
-    this.element.querySelector("h2")!.textContent = "";
-    this.element.querySelector("h3")!.textContent = "";
-    this.element.querySelector("p")!.textContent = "";
-
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent =
-      this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
